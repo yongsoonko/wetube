d99975f2e47b7f2bf99f7ad44d5ff0b4e2f5f537
Yongsoon Ko <goys5228@gmail.com>
2020-03-08 22:17:06 +0900

    미들웨어(파싱, 세션, 보안, 로깅) 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
-app.listen(4000, (req, res) => console.log(11));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get(
+  "/",
+  () => console.log("between"),
+  (req, res) => res.send("heelo")
+);
+
+app.listen(4000, (req, res) => console.log(1));
