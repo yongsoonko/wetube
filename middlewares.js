5d6b4570a02b12c48c04e72c7a9940addc0c4427
Yongsoon Ko <goys5228@gmail.com>
2020-03-15 23:49:02 +0900

    uploads 주소로 static file을 연결

--- middlewares.js
+++ middlewares.js
@@ -11,5 +11,5 @@ export const localsMiddleware = (req, res, next) => {
   next();
 };
 
-const multerVideo = multer({ dest: "videos/" });
+const multerVideo = multer({ dest: "uploads/videos/" });
 export const uploadVideo = multerVideo.single("videoFile");
